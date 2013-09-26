8c77958ab35000eda49064da17dffe48a8fdef07
nicodee <nicodemus.nutsukpui@meltwater.org>
2013-09-26 13:46:46 +0000

    burn/delete answer now works

--- scripts/js/home.js
+++ scripts/js/home.js
@@ -9,23 +9,30 @@ function perform_action() {
 		$(this).attr("disabled", "disabled");
 		$(this).css("color", "#444");
 		var action = $(this).attr("data-original-title").toLowerCase();
+		var panel = $(this).attr("data-panel");
+		
 
 		if(action=="burn"){
-			var panel = $(this).attr("data-panel");
-			var burnt_panel = $(".panel"+panel);
+			var burnt_panel = $(".panel."+panel);
 			console.log(burnt_panel.attr("class"));
+			burn(action, burnt_panel);
 		}
 
 		else if(action=="proceed"){
-			console.log($(this).attr("data-panel"));
+			var proceed_panel = $(".panel."+panel);
+			console.log(proceed_panel.attr("class"));
 		}
 
 		else if(action=="follow"){
-			console.log($(this).attr("data-panel"));
+			var follow_panel = $(".panel."+panel);
+			console.log(follow_panel.attr("class"));
 		}
 	})
 }
 
+function burn(action, burnt_panel) {
+	burnt_panel.remove();
+}
 
 function proceed () {
 
