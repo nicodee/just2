ea43a9ade441ad59424f5ee4dcf55e5ca382b3eb
Samuel A <elimence@gmail.com>
2013-09-27 14:39:48 +0000

    implemented inbox and 3d timeline

---
+++ scripts/js/inbox.js
@@ -0,0 +1,42 @@
+var flag = false;
+var ticks = $('.tk');
+
+$('#select_all').change(function(evt) {
+
+	if (flag) {
+		ticks.each(function() {
+			$(this).prop('checked',false);
+			flag = false;
+		});
+	}// end if
+
+	else {
+		ticks.each(function() {
+			$(this).prop('checked', true);
+			flag = true;
+		});
+	}// end else
+});
+
+
+$('.glyphicon').tooltip()
+
+
+$(document).ready(function() {
+	$('#details').hide();
+
+	$("tr").on("click",function(){
+		// $('#content').hide();
+		// $('#details').show();
+		$('#content').hide('slide', {direction: 'left'}, 1000);
+		$('#details').show('slide', {direction: 'right'}, 1000);
+	});
+
+	$('#back').on("click", function() {
+		$('#content').show('slide', {direction: 'left'}, 1000);
+		$('#details').hide('slide', {direction: 'right'}, 1000);
+	});
+});
+
+
+
